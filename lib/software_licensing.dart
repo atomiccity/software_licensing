0cc69ffd60a5ecb54f95031347b764035e14bde8
Michael R. Taylor <michael@atomic.city>
2025-01-30 00:41:29 -0600

    Updated the library export file

--- lib/software_licensing.dart
+++ lib/software_licensing.dart
@@ -1,6 +1,7 @@
 library;
 
 export 'src/software_license.dart';
+export 'src/software_licensing_activator.dart';
 export 'src/software_licensing_cache.dart';
 export 'src/software_licensing_client.dart';
 export 'src/software_licensing_validator.dart';
